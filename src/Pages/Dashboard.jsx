2c1489e4a6206a4147fa3b80f1fbeeedf6d4fa39
Admin <vaghela9632@gmail.co>
2024-04-19 12:27:41 +0530

    dashboard  error slove

--- src/Pages/Dashboard.jsx
+++ src/Pages/Dashboard.jsx
@@ -4,79 +4,68 @@ import { Link } from "react-router-dom";
 const Dashboard = () => {
   return (
     <div>
-      <div class="content-wrapper">
-        {/* <!-- Content Header (Page header) --> */}
-        <div class="content-header">
-          <div class="container-fluid">
-            <div class="row mb-2">
-              <div class="col-sm-6 text-left">
-                <h1 class="m-0">Dashboard</h1>
+      <div className="content-wrapper">
+        {/* Content Header (Page header) */}
+        <div className="content-header">
+          <div className="container-fluid">
+            <div className="row mb-2">
+              <div className="col-sm-6 text-left">
+                <h1 className="m-0">Dashboard</h1>
               </div>
-              {/* <!-- /.col --> */}
-              <div class="col-sm-6">
-                <ol class="breadcrumb float-sm-right">
-                  <li class="breadcrumb-item">
+              <div className="col-sm-6">
+                <ol className="breadcrumb float-sm-right">
+                  <li className="breadcrumb-item">
                     <Link to="/">Home</Link>
                   </li>
-                  <li class="breadcrumb-item active">Dashboard</li>
+                  <li className="breadcrumb-item active">Dashboard</li>
                 </ol>
               </div>
-              {/* <!-- /.col --> */}
             </div>
-            {/* <!-- /.row --> */}
           </div>
-          {/* <!-- /.container-fluid --> */}
         </div>
-        {/* <!-- /.content-header --> */}
 
-        {/* <!-- Main content --> */}
-        <section class="content">
-          <div class="container-fluid">
-            {/* <!-- Small boxes (Stat box) --> */}
-            <div class="row text-left">
-              <div class="col-lg-3 col-6">
-                {/* <!-- small box --> */}
-                <div class="small-box bg-info">
-                  <div class="inner">
+        {/* Main content */}
+        <section className="content">
+          <div className="container-fluid">
+            {/* Small boxes (Stat box) */}
+            <div className="row text-left">
+              <div className="col-lg-3 col-6">
+                {/* small box */}
+                <div className="small-box bg-info">
+                  <div className="inner">
                     <h3>0</h3>
-
-                    <p>New STL Files </p>
+                    <p>New STL Files</p>
                   </div>
-                  <div class="icon">
-                    <i class="ion ion-bag"></i>
+                  <div className="icon">
+                    <i className="ion ion-bag"></i>
                   </div>
-                  <Link to="/stl" class="small-box-footer">
-                    More info <i class="fas fa-arrow-circle-right"></i>
+                  <Link to="/stl" className="small-box-footer">
+                    More info <i className="fas fa-arrow-circle-right"></i>
                   </Link>
                 </div>
               </div>
-              {/* <!-- ./col --> */}
-              <div class="col-lg-3 col-6">
-                {/* <!-- small box --> */}
-                <div class="small-box bg-success">
-                  <div class="inner">
-                    <h3>
-                      0
-                    </h3>
-
+              {/* ./col */}
+              <div className="col-lg-3 col-6">
+                {/* small box */}
+                <div className="small-box bg-success">
+                  <div className="inner">
+                    <h3>0</h3>
                     <p>Reviewed files</p>
                   </div>
-                  <div class="icon">
-                    <i class="ion ion-stats-bars"></i>
+                  <div className="icon">
+                    <i className="ion ion-stats-bars"></i>
                   </div>
-                  <Link to="/stl" class="small-box-footer">
-                    More info <i class="fas fa-arrow-circle-right"></i>
+                  <Link to="/stl" className="small-box-footer">
+                    More info <i className="fas fa-arrow-circle-right"></i>
                   </Link>
                 </div>
               </div>
             </div>
-            {/* <!-- /.ro/w --> */}
-
-            {/* <!-- /.row (main row) --> */}
+            {/* /.row (main row) */}
           </div>
-          {/* <!-- /.container-fluid --> */}
+          {/* /.container-fluid */}
         </section>
-        {/* <!-- /.content --> */}
+        {/* /.content */}
       </div>
     </div>
   );
