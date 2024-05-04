cb7a8c64dc6f5ca609475e3c34bc8509595d2235
Yashpal-vaghela <vaghela9632@gmail.com>
2024-05-04 19:27:48 +0530

    apisetinevent-cat-edit

--- src/Pages/EventCatEdit.jsx
+++ src/Pages/EventCatEdit.jsx
@@ -2,26 +2,41 @@ import React, { useState, useEffect } from "react";
 import { Link, useParams } from "react-router-dom";
 import axios from "axios";
 import Swal from "sweetalert2";
+import Cookies from "js-cookie";
 
 const EventCatEdit = () => {
   const { id } = useParams(); // Assuming you have an id parameter for the event category
-
+  const [data, setData] = useState([]);
   // State variables to hold form data
   const [formData, setFormData] = useState({
     name: "",
-    image: "",
-    video: "",
+    image: null,
+    video: null,
   });
 
   useEffect(() => {
     axios
-      .get(`http://localhost:5000/eventCatagory/${id}`)
+      .post(`https://denticadentalstudio.com/api/show/event_category`,{id: id},
+      {
+        headers: {
+          "content-type": "application/json",
+          Authorization: `Bearer ${Cookies.get("token")}`,
+        },
+      }
+      )
       .then((res) => {
-        const data = res.data;
+        console.log(res.data.data.event_category);
+        const data = res.data.data.event_category;
+
+        if (Array.isArray(res.data)){
+          setData(res.data.data.event_category);
+        }else if(typeof res.data === "object"){
+          setData([res.data.data.event_category]);
+        }
         setFormData({
-          name: data.name,
-          image: data.image,
-          video: data.video,
+          name: res.data.name,
+          image: res.data.image,
+          video: res.data.video,
         });
       })
       .catch((error) => {
@@ -31,19 +46,27 @@ const EventCatEdit = () => {
 
   // Function to handle changes to form fields
   const handleChange = (e) => {
-    const { name, value } = e.target;
-    setFormData({
-      ...formData,
-      [name]: value,
-    });
+    const { name, value, files } = e.target;
+    if (name === "image" || name === "viseo"){
+      setFormData((prevData)=>({
+            ...prevData,
+            [name]: files[0] ,
+          }));
+    } else{
+      setFormData((prevData)=>({
+       ...prevData,
+        [name]: value,
+      }));
+    }
+    
   };
 
   // Function to handle form submission
   const handleSubmit = (e) => {
     e.preventDefault();
-    // Handle form submission here
+  
     axios
-      .put(`http://localhost:5000/eventCatagory/${id}`, formData)
+      .post(`https://denticadentalstudio.com/api/event_category/update ${id}`, formData)
       .then((res) => {
         console.log(res.data);
         Swal.fire({
@@ -101,52 +124,54 @@ const EventCatEdit = () => {
                     >
                       <h3 className="card-title">Edit Event Category</h3>
                     </div>
-                    <form onSubmit={handleSubmit} className="text-left">
-                      <div className="card-body">
-                        <div className="form-group">
-                          <label htmlFor="exampleInputName">Name</label>
-                          <input
-                            type="text"
-                            className="form-control"
-                            id="exampleInputName"
-                            name="name"
-                            value={formData.name}
-                            onChange={handleChange}
-                          />
+                    {data.map((ele)=>(
+                      <form onSubmit={handleSubmit} className="text-left" key={ele.id}>
+                        <div className="card-body">
+                          <div className="form-group">
+                            <label htmlFor="exampleInputName">Name</label>
+                            <input
+                              type="text"
+                              className="form-control"
+                              id="exampleInputName"
+                              name="name"
+                              value={formData.name}
+                              onChange={handleChange}
+                            />
+                          </div>
+                          <div className="form-group">
+                            <label htmlFor="exampleInputImage">Image</label>
+                            <input
+                              type="file"
+                              className="form-control"
+                              id="exampleInputImage"
+                              name="image"
+                              // value={ele.image}
+                              onChange={handleChange}
+                            />
+                          </div>
+                          <div className="form-group">
+                            <label htmlFor="exampleInputVideo">Video</label>
+                            <input
+                              type="file"
+                              className="form-control"
+                              id="exampleInputVideo"
+                              name="video"
+                              // value={ele.video}
+                              onChange={handleChange}
+                            />
+                          </div>
                         </div>
-                        <div className="form-group">
-                          <label htmlFor="exampleInputImage">Image</label>
-                          <input
-                            type="file"
-                            className="form-control"
-                            id="exampleInputImage"
-                            name="image"
-                            value={formData.image}
-                            onChange={handleChange}
-                          />
+                        <div className="card-footer">
+                          <button
+                            type="submit"
+                            className="btn btn-primary"
+                            style={{ backgroundColor: "#ca629d", border: "0" }}
+                          >
+                            Submit
+                          </button>
                         </div>
-                        <div className="form-group">
-                          <label htmlFor="exampleInputVideo">Video</label>
-                          <input
-                            type="file"
-                            className="form-control"
-                            id="exampleInputVideo"
-                            name="video"
-                            value={formData.video}
-                            onChange={handleChange}
-                          />
-                        </div>
-                      </div>
-                      <div className="card-footer">
-                        <button
-                          type="submit"
-                          className="btn btn-primary"
-                          style={{ backgroundColor: "#ca629d", border: "0" }}
-                        >
-                          Submit
-                        </button>
-                      </div>
-                    </form>
+                      </form>
+                    ))}
                   </div>
                 </div>
               </div>
